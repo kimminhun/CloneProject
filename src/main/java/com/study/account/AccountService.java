989d29d3bf59e65a55fed4e9fd5c8b8841191c95
minhun <asdfg6469597@gmail.com>
2022-01-10 00:59:12 +0900

    feat : 프로필 수정

--- src/main/java/com/study/account/AccountService.java
+++ src/main/java/com/study/account/AccountService.java
@@ -1,6 +1,7 @@
 package com.study.account;
 
 import com.study.domain.Account;
+import com.study.settings.Profile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -84,5 +85,13 @@ public class AccountService implements UserDetailsService {
         account.setEmailVerified(true);
         account.setJoinedAt(LocalDateTime.now());
         login(account);
+    }
+
+    public void updateProfile(Account account, Profile profile) {
+        account.setUrl(profile.getUrl());
+        account.setOccupation(profile.getOccupation());
+        account.setLocation(profile.getLocation());
+        account.setBio(profile.getBio());
+        accountRepository.save(account);
     }
 }
