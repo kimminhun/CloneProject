a5eec6acb8ff45597087cbe30c135bb6bec53445
minhun <asdfg6469597@gmail.com>
2022-01-12 00:59:11 +0900

    refactoring : mailsender 추상화

--- src/main/java/com/study/account/AccountService.java
+++ src/main/java/com/study/account/AccountService.java
@@ -1,13 +1,18 @@
 package com.study.account;
 
+import antlr.debug.MessageEvent;
 import com.study.domain.Account;
 import com.study.domain.Tag;
+import com.study.mail.EmailMessage;
+import com.study.mail.EmailService;
 import com.study.settings.form.Notifications;
 import com.study.settings.form.Profile;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,19 +23,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Slf4j
 @Transactional
 @Service
 @RequiredArgsConstructor
 public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final EmailService emailService;
     private final PasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
 
@@ -55,12 +63,15 @@ public class AccountService implements UserDetailsService {
     }
 
     public void sendSignUpConfirmEmail(Account newAccount) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디올래, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
-                "&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(newAccount.getEmail())
+                .subject("회원가입 인증")
+                .message("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+                        "&email=" + newAccount.getEmail())
+                .build();
+
+        emailService.sendEmail(emailMessage);
+
     }
 
     public void login(Account account) {
@@ -117,12 +128,16 @@ public class AccountService implements UserDetailsService {
 
 
     public void sendLoginLink(Account account) {
-        account.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(account.getEmail());
-        mailMessage.setSubject("스터디 올래, 로그인 링크");
-        mailMessage.setText("/login-by-email?token=" + account.getEmailCheckToken() + "&email=" + account.getEmail());
-        javaMailSender.send(mailMessage);
+
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(account.getEmail())
+                .subject("회원가입 인증")
+                .message("/check-email-token?token=" + account.getEmailCheckToken() +
+                        "&email=" + account.getEmail())
+                .build();
+
+        emailService.sendEmail(emailMessage);
+
     }
 
     public void addTag(Account account, Tag tag) {
---
+++ src/main/java/com/study/mail/EmailService.java
@@ -0,0 +1,6 @@
+package com.study.mail;
+
+public interface EmailService {
+
+    void sendEmail(EmailMessage emailMessage);
+}
